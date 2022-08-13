87b8396b6b1112bb68012a3d692100bfe86cef1a
novinbukannopin <68802465+novinbukannopin@users.noreply.github.com>
2022-08-14 01:44:42 +0700

    malming

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+
+// module.exports = {
+//   style: {
+//     postcss: {
+//       plugins: [
+//         require("tailwindcss")("./tailwind.config.js"),
+//         require("autoprefixer"),
+//       ],
+//     },
+//   },
+// };
