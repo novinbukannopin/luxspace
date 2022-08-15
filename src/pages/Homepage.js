bf4e71843dbdd8164528b414a6c67a9d27a5812b
novinbukannopin <68802465+novinbukannopin@users.noreply.github.com>
2022-08-15 12:00:50 +0700

    browseroom + justarrived add featuring fix

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../parts/Header";
 import Hero from "../parts/Hero";
+import BrowserRoom from "../parts/Homepage/BrowserRoom";
+import JustArrived from "../parts/Homepage/JustArrived";
 
 export default function Homepage(props) {
   return (
     <>
       <Header />
       <Hero />
+      <BrowserRoom />
+      <JustArrived />
     </>
   );
 }
